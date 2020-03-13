4d7f04427af6625c337bdec1a7675a92ea27b602
Tiago Duarte <tiago.figueiredo@g.globo>
2020-03-13 13:12:14 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,188 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Button from '@material-ui/core/Button';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import InfoIcon from '@material-ui/icons/Info';
+import CloseIcon from '@material-ui/icons/Close';
+import green from '@material-ui/core/colors/green';
+import amber from '@material-ui/core/colors/amber';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import WarningIcon from '@material-ui/icons/Warning';
+import { withStyles } from '@material-ui/core/styles';
+
+function UpdateButton(props) {
+  const { text } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Button size="small" style={{ color: "#FFF", fontWeight: "bold" }}>{text}</Button>
+  )
+}
+
+const variantIcon = {
+  success: CheckCircleIcon,
+  warning: WarningIcon,
+  error: ErrorIcon,
+  info: InfoIcon,
+};
+
+const styles1 = theme => ({
+  success: {
+    backgroundColor: green[600],
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  info: {
+    backgroundColor: theme.palette.primary.dark,
+  },
+  warning: {
+    backgroundColor: amber[700],
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing.unit,
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
+
+function MySnackbarContent(props) {
+  const { classes, className, message, onClose, variant, btnMSG, ...other } = props;
+  const Icon = variantIcon[variant];
+
+  return (
+    <SnackbarContent
+      className={classNames(classes[variant], className)}
+      aria-describedby="client-snackbar"
+      message={
+        <span id="client-snackbar" className={classes.message}>
+          <Icon className={classNames(classes.icon, classes.iconVariant)} />
+          {message}
+        </span>
+      }
+      action={[
+        btnMSG != undefined ? <UpdateButton text={btnMSG} /> : '',
+        <IconButton
+          key="close"
+          aria-label="Close"
+          color="inherit"
+          className={classes.close}
+          onClick={onClose}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <CloseIcon className={classes.icon} />
+        </IconButton>,
+      ]}
+      {...other}
+    />
   );
 }
 
-export default App;
+MySnackbarContent.propTypes = {
+  classes: PropTypes.object.isRequired,
+  className: PropTypes.string,
+  message: PropTypes.node,
+  onClose: PropTypes.func,
+  variant: PropTypes.oneOf(['success', 'warning', 'error', 'info']).isRequired,
+};
+
+const MySnackbarContentWrapper = withStyles(styles1)(MySnackbarContent);
+
+const styles2 = theme => ({
+  margin: {
+    margin: theme.spacing.unit,
+  },
+});
+
+class App extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleClick = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    const inMobile = false;
+
+    return (
+      <div>
+        <Button className={classes.margin} onClick={this.handleClick}>
+          Open success snackbar
+        </Button>
+
+
+        
+        <Snackbar
+          anchorOrigin={{
+            vertical: inMobile ? 'bottom' : 'top',
+            horizontal: inMobile ? 'left' : 'right',
+          }}
+          open={this.state.open}
+          autoHideDuration={5000}
+          onClose={this.handleClose}
+        >
+          <MySnackbarContentWrapper
+            onClose={this.handleClose}
+            variant="success"
+            message="This is a success message!"
+          />
+        </Snackbar>
+
+
+
+
+
+        <MySnackbarContentWrapper
+          variant="error"
+          className={classes.margin}
+          message="This is an error message! https://www.google.com/ https://www.google.com/ https://www.google.com/"
+          btnMSG="corrigir"
+        />
+        <MySnackbarContentWrapper
+          variant="warning"
+          className={classes.margin}
+          message="This is a warning message!"
+        />
+        <MySnackbarContentWrapper
+          variant="info"
+          className={classes.margin}
+          message="This is an information message!"
+          btnMSG="atualizar"
+        />
+        <MySnackbarContentWrapper
+          variant="success"
+          className={classes.margin}
+          message="This is a success message! This is a success message! This is a success message! This is a success message! This is a success message! This is a success message!"
+        />
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles2)(App);
+
